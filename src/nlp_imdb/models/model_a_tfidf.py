5cfc796dfd5543246d3937e26df8514748c045c9
rodrigo caceres valdes <rodrigocaceresvaldes@192.168.1.20>
2025-12-29 23:07:23 -0300

    chore: update files.

--- src/nlp_imdb/models/model_a_tfidf.py
+++ src/nlp_imdb/models/model_a_tfidf.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Tuple
+from typing import Tuple
 
 import joblib
 from sklearn.linear_model import LogisticRegression
